88fc98be251534d7bed90e14c7bcc7a5827e1ab2
jmaestas7 <jmaestas2570@gmail.com>
2017-09-06 15:15:55 -0700

    added updated script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,20 @@
 $( document ).ready(function() {
 
   $("#animal-add").on("click", function() {
+    debugger;
     var buttonRow = $("#animal-button");
     var input = $("#input").val();
     var newButton = $("<button>");
     newButton.attr("animal", input)
-    newButton.attr("id", "animal");
-    newButton.attr("class", "btn btn-dark");
-    newButton.attr("type", "animal");
+    newButton.attr("class", "btn btn-dark animal");
     newButton.html(input);
     buttonRow.append(newButton);
   });
 
-
-  $("button").on("click", function() {
-
-    var id = String($(this).attr("id"));
-
-    if (id === "animal") {
+  $(document).on("click", ".animal", function() {
 
       searchTag = $(this).attr("animal");
       var APIKey = "dc6zaTOxFJmzC";
-
 
       function getanimals(tag) {
         var queryURL = "http://api.giphy.com/v1/gifs/random?" + 
@@ -30,43 +23,42 @@ $( document ).ready(function() {
         $.ajax({
         url: queryURL,
         method: "GET"
-        })
+        }) 
         .done(function(response) {
           var imageUrl = response.data.image_original_url;
           var src = imageUrl.replace("/giphy.gif", "/200_s.gif");
           var animate = imageUrl.replace("/giphy.gif", "/200.gif");
-          console.log(animate);
           var image = $("<img>").attr("style", "width:225px; padding: 15px");
           image.attr("src", src);
           image.attr("data-still", src);
           image.attr("data-animate", animate);
           image.attr("data-state", "still");
-          image.attr("class", "image");
+          image.attr("class", "gif");
+          image.on("click", function() {
+          var state = $(this).attr("data-state");
+          console.log(state);
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+      });
           $("#images").append(image);
-          console.log($(image).attr("class"));
         });
       };
 
-      $( "#images").empty();
+      $("#images").empty();
 
       for (var i = 0; i < 10; i++) {
         getanimals(searchTag);
       }
-    }
-  });
-
-  $(".image").on("click", function() {
-    var state = $(this).attr("data-state");
-    console.log(state);
-    // if (state === "still") {
-    //   $(this).attr("src", $(this).attr("data-animate"));
-    //   $(this).attr("data-state", "animate");
-    // } else {
-    //   $(this).attr("src", $(this).attr("data-still"));
-    //   $(this).attr("data-state", "still");
     // }
   });
 });
+
+
 
 // <img src="http://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif"
 // data-still="http://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif"
